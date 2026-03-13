32241d99b660b7ac7df816761a6410d297e34438
khaderq79 <khaderqaabar79@gmail.com>
2026-03-13 13:25:11 +0200

    """""""""""

--- waynest-be/src/modules/place/place.module.ts
+++ waynest-be/src/modules/place/place.module.ts
@@ -3,9 +3,41 @@ import { PlaceService } from './place.service';
 import { PlaceController } from './place.controller';
 import { Place } from './entities/place.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Provider } from '../providers/entities/provider.entity';
+import { City } from '../cities/entities/city.entity';
+import { Tag } from '../tag/entities/tag.entity';
+import { Review } from '../review/entities/review.entity';
+import { Event } from '../event/entities/event.entity';
+import { PlacePricing } from '../placepricing/entities/placepricing.entity';
+import { PlaceOpeningHour } from '../place-opening-hours/entities/place-opening-hour.entity';
+import { ProvidersModule } from '../providers/providers.module';
+import { CitiesModule } from '../cities/cities.module';
+import { TagModule } from '../tag/tag.module';
+import { ReviewModule } from '../review/review.module';
+import { EventModule } from '../event/event.module';
+import { PlacepricingModule } from '../placepricing/placepricing.module';
+import { PlaceOpeningHoursModule } from '../place-opening-hours/place-opening-hours.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Place])],
+  imports: [
+    TypeOrmModule.forFeature([
+      Place,
+      Provider,
+      City,
+      Tag,
+      Review,
+      Event,
+      PlacePricing,
+      PlaceOpeningHour,
+    ]),
+    ProvidersModule,
+    CitiesModule,
+    TagModule,
+    ReviewModule,
+    EventModule,
+    PlacepricingModule,
+    PlaceOpeningHoursModule,
+  ],
   controllers: [PlaceController],
   providers: [PlaceService],
   exports: [PlaceService],
