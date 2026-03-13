18b2a9aee12eca4a1b77fb363614295631fbec09
khaderq79 <khaderqaabar79@gmail.com>
2026-03-13 12:06:01 +0200

    """""""""""

--- waynest-be/api/index.ts
+++ waynest-be/api/index.ts
@@ -1,4 +1,4 @@
-import 'tsconfig-paths/register';
+import 'module-alias/register';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../src/app.module';
 import express from 'express';
