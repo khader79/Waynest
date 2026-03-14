cdc0e669c0bb910997a663e87aa3917461f28982
khaderq79 <khaderqaabar79@gmail.com>
2026-03-14 15:56:13 +0200

    """"""""""""

--- waynest-be/src/main.ts
+++ waynest-be/src/main.ts
@@ -5,29 +5,21 @@ import cookieParser from 'cookie-parser';
 import { setDefaultResultOrder } from 'node:dns';
 
 async function bootstrap() {
-  // Prefer IPv4 to avoid IPv6-only SMTP connection failures on some networks
   try {
     setDefaultResultOrder('ipv4first');
-  } catch {
-    // Ignore if not supported by the Node version
-  }
+  } catch {}
 
   const app = await NestFactory.create(AppModule);
 
   app.use(cookieParser());
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://waynest-8lub.vercel.app'],
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept',
   });
-  app.enableCors({
-    origin: 'https://waynest-8lub.vercel.app/',
-    credentials: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Accept',
-  });
+
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT');
   //@ts-ignore
